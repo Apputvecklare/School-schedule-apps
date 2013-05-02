cb63fe799bb8dd88649a9cb5c000201cd3ed79b0
Apputvacklare <ashraf.iosdev@gmail.com>
2013-05-02 15:07:59 +0200

    Initial Commit

---
+++ SchoolSchemaApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SchoolSchemaApp
+//
+//  Created by Ashraf Jibrael on 5/2/13.
+//  Copyright (c) 2013 Ashraf Jibrael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
